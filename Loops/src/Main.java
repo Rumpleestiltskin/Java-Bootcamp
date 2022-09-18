a2d3b0d7a95b7dd9afabd61050e23902f2ac17d5
obeno <obeno@DESKTOP-NTLQ89K.local>
2022-09-18 17:25:05 +0300

    homework

---
+++ Loops/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//for
+		for(int i=1;i <= 10; i+=2) {
+			System.out.println(i);
+			
+		}
+		System.out.println("For Loop Over");
+		
+		//While
+		int i=1;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+			
+		}
+		System.out.println("While loop over	");
+		//do while
+		int j = 1;
+		do {
+			System.out.println(j);
+			j+=2;
+			}while(j<10);
+		System.out.println("Do while over");
+		
+
+	}
+
+}
