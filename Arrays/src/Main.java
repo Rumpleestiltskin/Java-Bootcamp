c102e20340a20c4af0031aeda960c2ebad75bab2
obeno <obeno@DESKTOP-NTLQ89K.local>
2022-09-18 17:17:13 +0300

    first commit

---
+++ Arrays/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[] students =	new String[3];
+		students[0] = "Engin";
+		students[1] = "Derin";
+		students[2] = "Salih";
+		
+		for(int i=0 ; i < students.length; i++) {
+			System.out.println(students[i]);
+		}
+		
+		System.out.println("-----------------------------");
+		
+		for(String student: students) {
+			System.out.println(student);
+		}
+			
+
+	}
+
+}
